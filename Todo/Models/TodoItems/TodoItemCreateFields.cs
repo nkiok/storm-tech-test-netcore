5a56e60ba395abf7ba89c4b174b0e6dbb9d9457a
nkiok <nikoskiok@gmail.com>
2018-11-18 19:47:10 +0000

    base class for TodoItem data annotations for display names

--- Todo/Models/TodoItems/TodoItemCreateFields.cs
+++ Todo/Models/TodoItems/TodoItemCreateFields.cs
@@ -2,21 +2,19 @@
 
 namespace Todo.Models.TodoItems
 {
-    public class TodoItemCreateFields
+    public class TodoItemCreateFields : TodoItemBase
     {
-        public int TodoListId { get; set; }
-        public string Title { get; set; }
-        public string TodoListTitle { get; set; }
-        public string ResponsiblePartyId { get; set; }
-        public Importance Importance { get; set; } = Importance.Medium;
-
         public TodoItemCreateFields() { }
 
         public TodoItemCreateFields(int todoListId, string todoListTitle, string responsiblePartyId)
         {
             TodoListId = todoListId;
+
             TodoListTitle = todoListTitle;
+
             ResponsiblePartyId = responsiblePartyId;
+
+            Importance = Importance.Medium;
         }
     }
 }
